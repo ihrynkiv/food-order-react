adaecd34ba35ec159cae31b9c55621b5818099ee
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2021-05-04 20:14:18 +0300

    Added a Meals Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/Layout/Header.js';
+import Meals from './components/Meals/Meals.js';
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
