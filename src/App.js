e23f8d845b33573d84ec3efddf1af37e11fdd9cb
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2021-05-05 09:27:01 +0300

    Added a Modal via a react-portal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cart from './components/Cart/Cart.js';
 import Header from './components/Layout/Header.js';
 import Meals from './components/Meals/Meals.js';
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
