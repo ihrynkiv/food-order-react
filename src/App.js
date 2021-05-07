172e8c1882d8f08f2733f1e7e5b089d4ffbdc879
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2021-05-07 16:08:43 +0300

    Add More Complex Reducer Logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import Cart from './components/Cart/Cart.js';
-import Header from './components/Layout/Header.js';
-import Meals from './components/Meals/Meals.js';
-import CartProvider from './store/CartProvider.js';
+import Cart from './components/Cart/Cart';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
