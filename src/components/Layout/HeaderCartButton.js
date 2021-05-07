fcac80aa7854175e3bbb27bc475ccd5966c3e755
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2021-05-07 17:19:22 +0300

    Add Media Queries

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -15,7 +15,7 @@ const HeaderCartButton = props => {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span className={classes['sm-hide']}>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
